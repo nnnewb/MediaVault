5ff0d845dc47d0a2ee566b8955041abdae2ca69c
weak_ptr <weak_ptr@163.com>
2024-12-20 17:26:31 +0800

    feat: init cobra project layout

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 weak_ptr <weak_ptr@163.com>
+This file is apart of the project media-vault. All rights reserved.
+*/
+package main
+
+import "gitee.com/uniqptr/media-vault.git/cmd"
+
+func main() {
+	cmd.Execute()
+}
