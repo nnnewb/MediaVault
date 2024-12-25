54c9d8dc588288690c901aa6c0da8a9244342887
weak_ptr <weak_ptr@163.com>
2024-12-25 18:12:11 +0800

    feat: 新增一个后台任务页面，基本骨架

---
+++ media-vault/src/views/TaskView.vue
@@ -0,0 +1,25 @@
+<script setup>
+const tasks = [
+    {
+        id: 1,
+        name: '提取封面',
+        status_string: 'finish',
+        progress: 1,
+        total: 1,
+    }
+];
+</script>
+
+<template>
+    <el-container direction="vertical">
+        <el-table :data="tasks">
+            <el-table-column prop="id" label="id" />
+            <el-table-column prop="name" label="名称" />
+            <el-table-column prop="status_string" label="状态" />
+            <el-table-column prop="progress" label="进度" />
+        </el-table>
+    </el-container>
+</template>
+
+<style scoped>
+</style>
