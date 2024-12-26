e489e8a26bb5b119dc3f15932307ca513addbeb7
weak_ptr <weak_ptr@163.com>
2024-12-27 00:17:06 +0800

    feat: 实现基本的路径选择器导航

--- internal/service/path.go
+++ internal/service/path.go
@@ -3,9 +3,13 @@ package service
 import (
 	"os"
 	"path/filepath"
+	"time"
 
-	"gitee.com/uniqptr/media-vault.git/internal/models"
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
+
+	"gitee.com/uniqptr/media-vault.git/internal/logging"
+	"gitee.com/uniqptr/media-vault.git/internal/models"
 )
 
 type PathService struct {
@@ -16,6 +20,10 @@ func NewPathService() *PathService {
 }
 
 func (s *PathService) ReadDir(path string) ([]models.PathEntry, error) {
+	if path == "" {
+		return s.ListRoot()
+	}
+
 	entries, err := os.ReadDir(path)
 	if err != nil {
 		return nil, errors.WithStack(err)
@@ -23,10 +31,21 @@ func (s *PathService) ReadDir(path string) ([]models.PathEntry, error) {
 
 	ret := make([]models.PathEntry, 0, len(entries))
 	for _, entry := range entries {
+		info, err := entry.Info()
+		if err != nil {
+			logging.GetLogger().Warn("can not get info of entry", zap.Error(err))
+		}
+
+		var updatedAt time.Time
+		if info != nil {
+			updatedAt = info.ModTime()
+		}
+
 		ret = append(ret, models.PathEntry{
-			Name:  entry.Name(),
-			IsDir: entry.IsDir(),
-			Path:  filepath.Join(path, entry.Name()),
+			Name:      entry.Name(),
+			IsDir:     entry.IsDir(),
+			Path:      filepath.Join(path, entry.Name()),
+			UpdatedAt: updatedAt,
 		})
 	}
 	return ret, nil
--- media-vault/src/components/PathSelectDialog.vue
+++ media-vault/src/components/PathSelectDialog.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { inject, defineProps } from 'vue';
+import { ArrowLeft, Check, Document, Folder, Refresh } from '@element-plus/icons-vue';
+import dayjs from 'dayjs';
+import { ElButton, ElButtonGroup, ElCol, ElContainer, ElDialog, ElIcon, ElInput, ElRow, ElTable, ElTableColumn, ElText, ElTooltip } from 'element-plus';
+import { defineProps, inject, reactive, ref } from 'vue';
 
 const props = defineProps({
     select_dir: {
@@ -9,30 +12,133 @@ const props = defineProps({
 })
 
 const axios = inject('axios');
-const tree_props = {
-    label: 'name',
-    isLeaf: item => !item.is_dir,
-};
+const choosed_path = ref('');
+const current_path = ref('');
+const entries = reactive([]);
+const history = reactive([]);
 
-function load_node(node, resolve, reject) {
-    if (!node.data.path) {
-        resolve([{
-            name: 'D:/',
-            is_dir: true,
-            path: 'D:/'
-        }])
-        return
-    }
-    axios.post('/api/v1/path/list', { path: node.data.path }).then(resp => {
-        resolve(resp.data.data)
-    }).catch(reject)
+function load_entries(path) {
+    axios.post('/api/v1/path/list', {
+        path: path,
+    }).then(resp => {
+        if (resp.data.code !== 0) {
+            console.log(resp.data);
+            return
+        }
+
+        entries.length = 0;
+        entries.push(...resp.data.data);
+    })
 }
+
+function choose_entry(row) {
+    if (row) {
+        choosed_path.value = row.path;
+    } else {
+        choosed_path.value = ''
+    }
+}
+
+function enter_dir(row) {
+    load_entries(row.path);
+    history.push(current_path.value);
+    current_path.value = row.path;
+    choosed_path.value = row.path;
+}
+
+function back() {
+    const cur = current_path.value;
+    const path = history.pop()
+    load_entries(path)
+    current_path.value = path;
+    choosed_path.value = cur;
+}
+
+function refresh() {
+    load_entries(current_path.value);
+    choosed_path.value = '';
+}
+
+load_entries('');
 </script>
 
 <template>
     <el-dialog title="选择路径" width="800">
-        <el-tree show-checkbox :load="load_node" :props="tree_props" lazy />
+        <el-container direction="vertical">
+            <el-row>
+                <el-col :span="24">
+                    <el-input v-model="current_path">
+                        <template #prepend>
+                            <el-button @click="back">
+                                <el-icon>
+                                    <arrow-left />
+                                </el-icon>
+                            </el-button>
+                        </template>
+
+                        <template #append>
+                            <el-button-group>
+                                <el-button @click="refresh">
+                                    <el-icon>
+                                        <Refresh />
+                                    </el-icon>
+                                </el-button>
+                            </el-button-group>
+                        </template>
+                    </el-input>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-col :span="24">
+                    <el-table :data="entries" highlight-current-row @current-change="choose_entry"
+                        @row-dblclick="enter_dir" :max-height="400">
+                        <el-table-column label="类型" width="80" sortable>
+                            <template #default="scope">
+                                <el-icon>
+                                    <folder v-if="scope.row.is_dir" />
+                                    <document v-else />
+                                </el-icon>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="name" label="文件名" sortable>
+                            <template #default="scope">
+                                <div style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                                    <el-tooltip placement="top" :content="scope.row.name">
+                                        <el-text style="user-select: none;">{{ scope.row.name }}</el-text>
+                                    </el-tooltip>
+                                </div>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="updated_at" label="修改时间" width="180" sortable>
+                            <template #default="scope">
+                                <el-text style="user-select: none;">{{ dayjs(scope.row.updated_at).format('YYYY-MM-DD HH:mm:ss') }}</el-text>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-col>
+            </el-row>
+
+            <el-row>
+                <el-col :span="20" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                    <el-input v-model="choosed_path" />
+                </el-col>
+
+                <el-col :push="1" :span="2">
+                    <el-button type="primary">
+                        <el-icon>
+                            <check />
+                        </el-icon>
+                        确定
+                    </el-button>
+                </el-col>
+            </el-row>
+        </el-container>
     </el-dialog>
 </template>
 
-<style scoped></style>
+<style scoped>
+.el-container {
+    gap: 7px;
+}
+</style>
