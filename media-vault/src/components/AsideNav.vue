0f6948476ed2e5e03262c33741aca08c7bdc0508
weak_ptr <weak_ptr@163.com>
2024-12-31 16:02:01 +0800

    fix: 导航菜单刷新后激活项目不对

--- media-vault/src/components/AsideNav.vue
+++ media-vault/src/components/AsideNav.vue
@@ -1,27 +1,31 @@
 <script setup>
-import { ElIcon, ElMenu, ElMenuItem } from "element-plus";
-import { Suitcase, VideoCamera, VideoCameraFilled } from "@element-plus/icons-vue";
+import { onMounted, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+
+const default_active = ref("/");
+const routes = [
+  { path: "/", label: "媒体库", icon: "VideoCamera" },
+  { path: "/anime", label: "动画", icon: "VideoCameraFilled" },
+  { path: "/task", label: "后台任务", icon: "Suitcase" },
+];
+
+watch(useRoute(), (route) => {
+  console.log(route.path);
+  default_active.value = route.path;
+});
+
+onMounted(() => {
+  default_active.value = useRoute().path;
+});
 </script>
 
 <template>
-  <el-menu :router="true" default-active="1" class="aside-menu">
-    <el-menu-item route="/" index="1">
+  <el-menu mode="vertical" :router="true" :default-active="default_active" class="aside-menu">
+    <el-menu-item :route="route.path" :index="route.path" v-for="route in routes">
       <el-icon>
-        <video-camera />
+        <component :is="route.icon" />
       </el-icon>
-      媒体库
-    </el-menu-item>
-    <el-menu-item route="/anime" index="2">
-      <el-icon>
-        <video-camera-filled />
-      </el-icon>
-      动画
-    </el-menu-item>
-    <el-menu-item route="/task" index="3">
-      <el-icon>
-        <suitcase />
-      </el-icon>
-      后台任务
+      {{ route.label }}
     </el-menu-item>
   </el-menu>
 </template>
