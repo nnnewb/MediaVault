45a3b7c983a1c94e3c96010d118e7ba56d07d11e
nnnewb <weak_ptr@outlook.com>
2025-03-12 12:17:29 +0800

    feat: 实现 /api/v1/task/list 接口

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -39,17 +41,52 @@ type Task struct {
 	Category         string     `gorm:"category"`
 	ProgressComplete int        `gorm:"progress_complete"`
 	ProgressTotal    int        `gorm:"progress_total"`
+	Tags             []TaskTag
 }
 
 func (*Task) TableName() string {
 	return "task"
 }
 
+func (t *Task) ToDTO() *TaskDTO {
+	dto := &TaskDTO{}
+	t.AsDTO(dto)
+	return dto
+}
+
+func (t *Task) AsDTO(dto *TaskDTO) {
+	dto.ID = t.ID
+	dto.CreatedAt = t.CreatedAt
+	dto.UpdatedAt = t.UpdatedAt
+	dto.Name = t.Name
+	dto.Description = t.Description
+	dto.StatusString = t.StatusString
+	dto.Status = t.Status
+	dto.Category = t.Category
+	dto.ProgressComplete = t.ProgressComplete
+	dto.ProgressTotal = t.ProgressTotal
+	dto.Tags = t.Tags
+}
+
 type TaskTag struct {
-	TaskID  uint   `gorm:"task_id"`
-	TaskTag string `gorm:"task_tag"`
+	TaskID  uint   `gorm:"task_id" json:"-"`
+	TaskTag string `gorm:"task_tag" json:"task_tag"`
 }
 
 func (*TaskTag) TableName() string {
 	return "task_tag"
+}
+
+type TaskDTO struct {
+	ID               uint       `json:"id"`
+	CreatedAt        time.Time  `json:"created_at"`
+	UpdatedAt        time.Time  `json:"updated_at"`
+	Name             string     `json:"name"`
+	Description      string     `json:"description"`
+	StatusString     string     `json:"status_string"`
+	Status           TaskStatus `json:"status"`
+	Category         string     `json:"category"`
+	ProgressComplete int        `json:"progress_complete"`
+	ProgressTotal    int        `json:"progress_total"`
+	Tags             []TaskTag  `json:"tags"`
 }
