38366d4bb8853b3d55ba6cd14c040bc74bb98717
nnnewb <weak_ptr@outlook.com>
2025-03-12 12:17:33 +0800

    refactor: 清理 choose_single 方法

--- media-vault/src/components/ChooseMedia/index.js
+++ media-vault/src/components/ChooseMedia/index.js
@@ -1,13 +1,11 @@
-import { h, ref, render } from "vue";
+import { h, render } from "vue";
 import ChooseMediaComponent from "./ChooseMedia.vue";
 
 function choose_single() {
   return new Promise((resolve, reject) => {
-    const result = ref(null);
     const container = document.createElement("div");
     const v_node = h(ChooseMediaComponent, {
       onConfirm: (choosed) => {
-        result.value = choosed;
         container.remove();
         resolve(choosed);
       },
@@ -16,7 +14,6 @@ function choose_single() {
         reject("cancelled");
       },
     });
-    console.log(ChooseMediaComponent._context);
     v_node.appContext = ChooseMediaComponent._context;
     render(v_node, container);
     document.body.appendChild(container);
