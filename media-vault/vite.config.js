52053e04d0d470709035a036169592adfd3247a1
nnnewb <weak_ptr@outlook.com>
2025-03-12 12:17:31 +0800

    fix: 自动导入 vue/vue-router 常用函数

--- media-vault/vite.config.js
+++ media-vault/vite.config.js
@@ -1,27 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:39876',
+      "/api": {
+        target: "http://127.0.0.1:39876",
         changeOrigin: true,
-      }
-    }
+      },
+    },
   },
   plugins: [
     vue(),
     vueDevTools(),
     AutoImport({
+      imports: ["vue", "vue-router"],
       resolvers: [ElementPlusResolver()],
     }),
     Components({
@@ -30,7 +31,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
