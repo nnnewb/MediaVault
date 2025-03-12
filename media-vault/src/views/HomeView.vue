e5dbb1d664100a309f0669d9a4d1cac4868bb6c4
nnnewb <weak_ptr@outlook.com>
2025-03-12 12:17:25 +0800

    refactor: 出于简便直观考虑，媒体库先使用表格形式

--- media-vault/src/views/HomeView.vue
+++ media-vault/src/views/HomeView.vue
@@ -5,6 +5,9 @@ const page = ref(1);
 const page_size = ref(10);
 const axios = inject('axios');
 
+const searchForm = reactive({
+  search: '',
+});
 const medias = reactive([]);
 
 axios.post('/api/v1/media/list', { 'page': page.value, 'page_size': page_size.value, }).then(resp => {
@@ -18,10 +21,23 @@ axios.post('/api/v1/media/list', { 'page': page.value, 'page_size': page_size.va
 </script>
 
 <template>
-  <el-container>
-    <el-row class="row">
-      <video-item v-for="media in medias" :media="media" />
+  <el-container direction="vertical">
+    <el-row>
+      <el-form :inline="true" :model="searchForm">
+        <el-form-item>
+          <el-input type="text" placeholder="输入开始搜索" v-model="searchForm.search" prefix-icon="Search" clearable />
+        </el-form-item>
+      </el-form>
     </el-row>
+
+    <el-table :data="medias">
+      <el-table-column prop="id" label="id" width="100" />
+      <el-table-column prop="created_at" label="创建时间" width="300" />
+      <el-table-column prop="updated_at" label="更新时间" width="300" />
+      <el-table-column prop="information_id" label="信息id" width="100" />
+      <el-table-column prop="cover_id" label="封面id" width="100" />
+      <el-table-column prop="path" label="路径" />
+    </el-table>
   </el-container>
 </template>
 
